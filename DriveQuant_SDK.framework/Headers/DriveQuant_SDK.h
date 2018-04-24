d07ab29a88566b51bd90844b613815b0cd37ee3e
Jérémy Bayle <jeremy.bayle@drivequant.com>
2018-04-24 17:23:36 +0200

    Update version with no live data

--- DriveQuant_SDK.framework/Headers/DriveQuant_SDK.h
+++ DriveQuant_SDK.framework/Headers/DriveQuant_SDK.h
@@ -14,5 +14,4 @@ FOUNDATION_EXPORT double DriveQuant_SDKVersionNumber;
 //! Project version string for DriveQuant_SDK.
 FOUNDATION_EXPORT const unsigned char DriveQuant_SDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <DriveQuant_SDK/PublicHeader.h>
-#import <DriveQuant_SDK/SeeGateway.h>
+
