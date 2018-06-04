9912facaedd9a709f8b761da5cc376deb7c9f71b
Jérémy Bayle <jeremy.bayle@drivequant.com>
2018-06-04 10:13:15 +0200

    Build version 3.0.0-beta20

--- DriveQuant_SDK.framework/Headers/DriveQuant_SDK-Swift.h
+++ DriveQuant_SDK.framework/Headers/DriveQuant_SDK-Swift.h
@@ -236,17 +236,17 @@ SWIFT_CLASS("_TtC14DriveQuant_SDK22DriveQuantTripAnalysis")
 @interface DriveQuantTripAnalysis : NSObject
 - (void)setVehicle:(Vehicle * _Nonnull)vehicle;
 + (void)setEnableSharePos:(BOOL)enableSharePos;
-+ (void)initializeSDK:(Account * _Nonnull)account listener:(id <DriveQuantProtocol> _Nonnull)listener beaconRequired:(BOOL)beaconRequired timeOutDuration:(NSInteger)timeOutDuration;
++ (void)initializeSDK:(Account * _Nonnull)account listener:(id <DriveQuantProtocol> _Nonnull)listener recorderListener:(id <PostGenericProtocol> _Nonnull)recorderListener beaconRequired:(BOOL)beaconRequired timeOutDuration:(NSInteger)timeOutDuration;
 + (void)checkTripToRepost;
-+ (void)activateAutoStart:(id <PostGenericProtocol> _Nonnull)listener userId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle;
-+ (void)activateAutoStart:(id <PostGenericProtocol> _Nonnull)listener userId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions;
-+ (void)activateAutoStart:(id <PostGenericProtocol> _Nonnull)listener userId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle enableLive:(BOOL)enableLive;
-+ (void)activateAutoStart:(id <PostGenericProtocol> _Nonnull)listener userId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions enableLive:(BOOL)enableLive;
++ (void)activateAutoStartWithUserId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle;
++ (void)activateAutoStartWithUserId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions;
++ (void)activateAutoStartWithUserId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle enableLive:(BOOL)enableLive;
++ (void)activateAutoStartWithUserId:(NSString * _Nonnull)userId vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions enableLive:(BOOL)enableLive;
 + (void)deactivateAutoStart;
-+ (BOOL)start:(id <PostGenericProtocol> _Nonnull)listener vehicle:(Vehicle * _Nonnull)vehicle SWIFT_WARN_UNUSED_RESULT;
-+ (BOOL)start:(id <PostGenericProtocol> _Nonnull)listener vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions SWIFT_WARN_UNUSED_RESULT;
-+ (BOOL)start:(id <PostGenericProtocol> _Nonnull)listener vehicle:(Vehicle * _Nonnull)vehicle enableLive:(BOOL)enableLive SWIFT_WARN_UNUSED_RESULT;
-+ (BOOL)start:(id <PostGenericProtocol> _Nonnull)listener vehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions enableLive:(BOOL)enableLive SWIFT_WARN_UNUSED_RESULT;
++ (BOOL)startWithVehicle:(Vehicle * _Nonnull)vehicle SWIFT_WARN_UNUSED_RESULT;
++ (BOOL)startWithVehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions SWIFT_WARN_UNUSED_RESULT;
++ (BOOL)startWithVehicle:(Vehicle * _Nonnull)vehicle enableLive:(BOOL)enableLive SWIFT_WARN_UNUSED_RESULT;
++ (BOOL)startWithVehicle:(Vehicle * _Nonnull)vehicle beaconRegions:(NSArray<CLBeaconRegion *> * _Nullable)beaconRegions enableLive:(BOOL)enableLive SWIFT_WARN_UNUSED_RESULT;
 + (BOOL)stop SWIFT_WARN_UNUSED_RESULT;
 + (BOOL)isTripRunning SWIFT_WARN_UNUSED_RESULT;
 + (void)disableSDKWithMinutes:(NSInteger)minutes;
